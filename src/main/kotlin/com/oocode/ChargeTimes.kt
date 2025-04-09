1ccf42d8a183d0d5d874ca71aa5d0cc8f0dc3938
Ed <mredwardflack@gmail.com>
2025-04-09 12:06:20 +0100

    initial

--- src/main/kotlin/com/oocode/ChargeTimes.kt
+++ src/main/kotlin/com/oocode/ChargeTimes.kt
@@ -14,21 +14,27 @@ import java.time.format.DateTimeFormatter.RFC_1123_DATE_TIME
 
 fun main() {
     val weatherData = getWeatherDataFromAPI()
-    println(getTopThreeWind(weatherData))
+    println(getTopThreeWind(weatherData, mutableListOf(WeatherFields.EMBEDDED_WIND_FORECAST, WeatherFields.EMBEDDED_SOLAR_FORECAST)))
 }
 
-fun getTopThreeWind(weatherData: String): String {
+fun getTopThreeWind(weatherData: String, fieldsToSortBy: List<WeatherFields>): String {
     val weather = getWeatherRows(weatherData)
-    return "Best times to plug in:\n" + topThreeRowsFormatted(weather)
-        .joinToString("\n")
+    return "Best times to plug in:\n" + topThreeRowsFormatted(weather, fieldsToSortBy).joinToString("\n")
 }
 
-private fun topThreeRowsFormatted(lines: List<Weather>) = lines
-    .sortedByDescending { it.embeddedWindForecast.toInt() }
-    .take(3)
-    .map {  row -> createZonedDateTime(row.dateGmt, row.timeGmt) }
-    .sorted()
-    .map { it.format(RFC_1123_DATE_TIME) }
+private fun topThreeRowsFormatted(lines: List<Weather>, fieldsToSortBy: List<WeatherFields>): List<String> {
+    if (fieldsToSortBy.isNotEmpty()) {
+       return lines.sortedByDescending {it.getFieldAccumulated(fieldsToSortBy)}
+           .take(3)
+            .map { row -> createZonedDateTime(row.dateGmt, row.timeGmt) }
+            .sorted()
+            .map { it.format(RFC_1123_DATE_TIME)}
+    }
+    return lines.take(3)
+        .map { row -> createZonedDateTime(row.dateGmt, row.timeGmt) }
+        .sorted()
+        .map { it.format(RFC_1123_DATE_TIME) }
+}
 
 private fun createZonedDateTime(date: String, time: String): ZonedDateTime = ZonedDateTime.of(
     LocalDateTime.parse(date)
--- src/main/kotlin/com/oocode/Weather.kt
+++ src/main/kotlin/com/oocode/Weather.kt
@@ -1,4 +1,16 @@
 package com.oocode
+
+enum class WeatherFields {
+    DATE_GMT,
+    TIME_GMT,
+    SETTLEMENT_DATE,
+    SETTLEMENT_PERIOD,
+    EMBEDDED_WIND_FORECAST,
+    EMBEDDED_WIND_CAPACITY,
+    EMBEDDED_SOLAR_FORECAST,
+    EMBEDDED_SOLAR_CAPACITY
+}
+
 class Weather(
     val dateGmt: String,
     val timeGmt: String,
@@ -9,6 +21,27 @@ class Weather(
     val embeddedSolarForecast: String,
     val embeddedSolarCapacity: String
 ) {
+    fun getField(weatherField: WeatherFields): String {
+        when (weatherField) {
+            WeatherFields.DATE_GMT -> return this.dateGmt
+            WeatherFields.TIME_GMT -> return this.timeGmt
+            WeatherFields.SETTLEMENT_DATE -> return this.settlementDate
+            WeatherFields.SETTLEMENT_PERIOD -> return this.settlementPeriod
+            WeatherFields.EMBEDDED_WIND_FORECAST -> return this.embeddedWindForecast
+            WeatherFields.EMBEDDED_WIND_CAPACITY -> return this.embeddedWindCapacity
+            WeatherFields.EMBEDDED_SOLAR_FORECAST -> return this.embeddedSolarForecast
+            WeatherFields.EMBEDDED_SOLAR_CAPACITY -> return this.embeddedSolarCapacity
+        }
+    }
+
+    fun getFieldAccumulated(weatherFields: List<WeatherFields>): Int {
+        var total = 0
+        for (weatherField in weatherFields) {
+            total += this.getField(weatherField).toInt()
+        }
+        return total
+    }
+
     companion object {
         fun of(row: Array<String>): Weather {
             return Weather(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7])
